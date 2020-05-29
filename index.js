702fe7b40d770752cba104520444415351c1d147
kiran <kiran111candy@gmail.com>
2020-05-29 13:04:56 -0400

    deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 
-const PORT =  process.env.PORT || 4000;
+const PORT =  process.env.PORT;
 
 // serve static files
 app.use(express.static(__dirname + "/public"));
