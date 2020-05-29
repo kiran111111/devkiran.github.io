7a7db7b0f38d87a09343f7298a7761ac778e9ca4
kiran <kiran111candy@gmail.com>
2020-05-29 12:49:24 -0400

    add more projects

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 
-const PORT =  process.env.PORT;
+const PORT =  process.env.PORT || 4000;
 
 // serve static files
 app.use(express.static(__dirname + "/public"));
